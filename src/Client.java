17f230a36a9a998e4d3ba3ad1e9f62c0ebd5ff3e
Rafal <rniedziolkad@gmail.com>
2023-01-05 18:25:12 +0100

    Sending data on Client

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private static final int SERVER_PORT = 54321;
@@ -10,6 +12,13 @@ public class Client {
             System.out.println("Connected!");
             System.out.println(socket.getInetAddress() + ":" + socket.getPort());
             System.out.println("On local port: "+socket.getLocalPort());
+            Scanner scanner = new Scanner(System.in);
+            String message = "Random Chat Protocol";
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            do {
+                writer.println(message);
+                message = scanner.nextLine();
+            } while (!message.equals("quit"));
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
